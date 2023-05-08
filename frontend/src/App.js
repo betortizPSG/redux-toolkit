1d4355e5f731d3f433b8124e624f17626cf665d8
betortizPSG <betortiz_PSG@outlook.com.br>
2023-05-08 10:32:44 -0400

    arquivos copiados do projeto redux-thunk

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './css/task.css';
+import TaskForm from './components/TaskForm';
+
+function App() {
+
+  return (
+    <div>
+      <TaskForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/TaskForm.jsx
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from "react";
+import store from "../store/store";
+import { useDispatch } from "react-redux";
+import { addTask, removeTask, completedTask } from "../store/task/action";
+
+const TaskForm = () => {
+  const dispatch = useDispatch();
+
+  const [task, setTask] = useState("");
+
+  useEffect(() => {
+    store.subscribe(() => {
+      console.log(store.getState());
+    });
+  }, [])
+
+  
+
+  const handleAddTask = (e) => {
+    e.preventDefault();
+    dispatch(addTask(task));
+    setTask("");
+  };
+
+  const handleRemoveTask = (id) => {
+    dispatch(removeTask({ id}));
+  };
+
+  const handleCompletedTask = (id) => {
+    dispatch(completedTask({ id }));
+  };
+
+  return (
+    <>
+      <div className="container">
+        <h1>Teste Redux - Tarefas</h1>
+        <form onSubmit={handleAddTask}>
+          <input
+            type="text"
+            onChange={(e) => setTask(e.target.value)}
+            value={task}
+          />
+          <button type="submit">Adicionar</button>
+        </form>
+        {store.getState().map((task, index) => (
+          <div key={index}>
+            <h3>
+              {task.id} - {task.task}
+              <button onClick={() => handleRemoveTask(task.id)}>
+                Remover
+              </button>
+              <button onClick={() => handleCompletedTask(task.id)}>
+                Completar
+              </button>
+            </h3>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default TaskForm;
